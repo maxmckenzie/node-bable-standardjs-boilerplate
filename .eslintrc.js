48c9aee5090bc35dbf91b9d203dc143d1fa6f8cd
Max McKenzie <hello@maxmckenzie.uk>
2017-02-11 14:51:03 +0100

    added direnv .envrc

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "standard",
+    "plugins": [
+      "standard",
+      "promise"
+    ],
+    "globals"   : {
+      /* MOCHA */
+      "describe"   : false,
+      "it"         : false,
+      "before"     : false,
+      "beforeEach" : false,
+      "after"      : false,
+      "afterEach"  : false
+  }
+};
